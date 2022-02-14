a799a87e2622915faaee701e479f6f4f176c3ab3
Erik Kopcha <erickopcha@gmail.com>
2022-02-14 18:25:37 +0200

    update

---
+++ pluminite-ui/src/components/app/App.tsx
@@ -0,0 +1,16 @@
+import { Component } from "react"
+import Marketplace from "../../pages/marketplace/Marketplace";
+
+// import styles from './App.module.css';
+
+class App extends Component<any, any> {
+  render() {
+    return (
+      <>
+        <Marketplace />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ pluminite-ui/src/pages/marketplace/Marketplace.tsx
@@ -0,0 +1,15 @@
+import styles from './marketplace.module.css';
+import { Component } from "react";
+
+class Marketplace extends Component<any, any> {
+  render() {
+    return (
+      <>
+        <h1 className={styles.title}>Marketplace</h1>
+        <p>page</p>
+      </>
+    )
+  }
+}
+
+export default Marketplace;
