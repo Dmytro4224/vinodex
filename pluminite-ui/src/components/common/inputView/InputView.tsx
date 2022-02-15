22742322b709ff3307806033e1a04343bbf50a07
Erik Kopcha <erickopcha@gmail.com>
2022-02-15 11:38:55 +0200

    update

--- pluminite-ui/src/components/common/inputView/InputView.tsx
+++ pluminite-ui/src/components/common/inputView/InputView.tsx
@@ -2,8 +2,11 @@ import {ChangeEvent, Component} from "react";
 import styles from './inputView.module.css';
 
 interface IInputView {
-  placeholder: string;
   onChange: (e: ChangeEvent) => void;
+  absPlaceholder?: string;
+  placeholder?: string;
+  alt?: string;
+  icon?: any;
 };
 
 class InputView extends Component<Readonly<IInputView>> {
@@ -15,17 +18,34 @@ class InputView extends Component<Readonly<IInputView>> {
     return this.props.placeholder;
   }
 
+  private get absPlaceholder() {
+    return this.props.absPlaceholder;
+  }
+
+  private get icon() {
+    return this.props.icon;
+  }
+
+  private get alt() {
+    return this.props.alt;
+  }
+
   private onChange = async (e: ChangeEvent) => {
     this.props.onChange(e);
   }
 
   public render() {
     return (
-      <input
-        onChange={this.onChange}
-        placeholder={this.placeholder}
-        className={styles.inputView}
-        type="text"/>
+      <div className={styles.inputWrap}>
+        {this.icon && <img className={styles.icon} src={this.icon} alt={this.alt} />}
+        <input
+          onChange={this.onChange}
+          placeholder={this.placeholder}
+          className={styles.inputView}
+          type="text"
+        />
+        { this.absPlaceholder && <span className={styles.absPlaceholder}>{this.absPlaceholder}</span> }
+      </div>
     )
   }
 }
--- pluminite-ui/src/pages/marketplace/Marketplace.tsx
+++ pluminite-ui/src/pages/marketplace/Marketplace.tsx
@@ -1,15 +1,17 @@
 import { Component } from "react";
 import { InputView } from "../../components/common/inputView/InputView";
+import searchIcon from '../../assets/icons/search.svg';
 
 class Marketplace extends Component {
   render() {
     return (
       <>
         <InputView
-          onChange={(e) => {
-            console.log(e)
-          }}
-          placeholder={'Search'}/>
+          onChange={(e) => {console.log(e)}}
+          placeholder={'Search'}
+          icon={searchIcon}
+          absPlaceholder={'Search'}
+        />
       </>
     )
   }
