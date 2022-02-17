db351f61929c07c62bc375dc3d394943397a508c
IvanRosokha <54767244+rssites@users.noreply.github.com>
2022-02-17 18:19:27 +0200

    Правки

--- pluminite-ui/src/pages/home/Home.tsx
+++ pluminite-ui/src/pages/home/Home.tsx
@@ -85,6 +85,23 @@ class Home extends Component {
                                                 }}/>;
                         })}/>
 
+        <div className="d-flex flex-wrap flex-gap-36 mt-5">
+          {[{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}, {id: 6}].map(item => {
+            return <TokenCardView key={item.id}
+                                  countL={item.id}
+                                  countR={10}
+                                  days={'121 days left'}
+                                  name={'Item Name'}
+                                  author={'Creat name'}
+                                  likesCount={99}
+                                  isSmall={true}
+                                  buttonText={'Place a bid 0.08 ETH'}
+                                  onClick={() => {
+                                    alert('buy Place a bid 0.08 ETH');
+                                  }}/>;
+          })}
+        </div>
+
         {/*<Marketplace />
         <Marketplace2 />*/}
       </div>
