430a46731667dba547651e11a263207b9e34540f
Erik Kopcha <erickopcha@gmail.com>
2022-02-18 15:50:22 +0200

    update

--- pluminite-ui/src/components/app/App.tsx
+++ pluminite-ui/src/components/app/App.tsx
@@ -5,10 +5,10 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from "../../pages/home/Home";
-import { Header } from "../header/Header";
-import { UserProfile } from '../../pages/userProfile/UserProfile';
-import OrderDetail from '../../pages/orderDetail/orderDetail';
-
+import Header from "../header/Header";
+import UserProfile from '../../pages/userProfile/UserProfile';
+// import OrderDetail from '../../pages/orderDetail/orderDetail';
+  
 class App extends Component {
   //static contextType = null;
   render() {
@@ -22,7 +22,7 @@ class App extends Component {
             <Route path="/" element={<Home />} />
             <Route path="/artists/*" element={<p>ARTISTS PAGE</p>} />
             <Route path="/userProfile/:userId" element={<UserProfile />} />
-            <Route path="/token/:tokenId" element={<OrderDetail />} />
+            {/*<Route path="/token/:tokenId" element={<OrderDetail />} />*/}
           </Routes>
         </main>
 
