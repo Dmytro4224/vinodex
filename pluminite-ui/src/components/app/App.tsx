b3fa887b8d3fd7365323365ab77fe6526125887d
IvanRosokha <54767244+rssites@users.noreply.github.com>
2022-02-18 15:34:13 +0200

    Update App.tsx

--- pluminite-ui/src/components/app/App.tsx
+++ pluminite-ui/src/components/app/App.tsx
@@ -4,9 +4,10 @@ import { NftContractContext } from '../../contexts';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Navigate, Route, Routes } from 'react-router-dom';
+import Home from "../../pages/home/Home";
+import { Header } from "../header/Header";
+import { UserProfile } from '../../pages/userProfile/UserProfile';
 import OrderDetail from '../../pages/orderDetail/orderDetail';
-import Header from "../header/Header";
-import UserProfile from '../../pages/userProfile/UserProfile';
 
 class App extends Component {
   //static contextType = null;
@@ -15,11 +16,11 @@ class App extends Component {
       <>
         <Header />
 
-        <main>
+        <main className="container">
           <Routes>
             <Route path="*" element={<Navigate to="/" />} />
             <Route path="/" element={<Home />} />
-            <Route path="/artists" element={<p>ARTISTS PAGE</p>} />
+            <Route path="/artists/*" element={<p>ARTISTS PAGE</p>} />
             <Route path="/userProfile/:userId" element={<UserProfile />} />
             <Route path="/token/:tokenId" element={<OrderDetail />} />
           </Routes>
