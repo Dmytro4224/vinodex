99bc2e59917f8e7d86b589776e694fba63e53fc8
IvanRosokha <54767244+rssites@users.noreply.github.com>
2022-02-21 16:14:34 +0200

    Revert "details"
    This reverts commit c9add48694c297f698f8112f82743c0589135b40.

--- pluminite-ui/src/components/tokenDetail/tabs/detail/tokenDetailView.tsx
+++ pluminite-ui/src/components/tokenDetail/tabs/detail/tokenDetailView.tsx
@@ -1,11 +1,23 @@
 import { Component } from "react";
-import { ButtonCopy } from "../../../common/buttonCopy/ButtonCopy";
-import { IdentificationCopy } from "../../../common/identificationCopy/IdentificationCopy";
-import styles from './tokenDetailView.module.css';
+
+interface ITokenDetailItemView{
+
+}
 
 interface ITokenDetailView{
-  address: string,
-  id: string
+
+}
+
+class TokenDetailItemView extends Component<ITokenDetailItemView>{
+  constructor(props: ITokenDetailItemView) {
+    super(props);
+  }
+
+  render(){
+    return (
+      <div></div>
+    )
+  }
 }
 
 class TokenDetailView extends Component<ITokenDetailView>{
@@ -13,40 +25,9 @@ class TokenDetailView extends Component<ITokenDetailView>{
     super(props);
   }
 
-  private get address(){
-    return this.props.address;
-  }
-
-  private get id(){
-    return this.props.id;
-  }
-
   render(){
     return (
-        <div className={styles.detailWrap}>
-          <div className={styles.detailItem}>
-            <div className={styles.info}>
-                <h5 className={styles.title}>{this.address}</h5>
-                <p className={styles.ex}>example</p>
-            </div>
-            <ButtonCopy
-              onClick={() => { }}
-              copyText={this.address}
-            />
-          </div>
-          <div className={styles.detailItem}>
-            <div className={styles.info}>
-              <h5 className={styles.title}>{this.id}</h5>
-              <p className={styles.ex}>example</p>
-            </div>
-            <ButtonCopy
-              onClick={() => { }}
-              copyText={this.id}
-            />
-          </div>
-        </div>
+      <TokenDetailItemView />
     )
   }
 }
-
-export { TokenDetailView }
