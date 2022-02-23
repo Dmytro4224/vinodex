a3aae3abbc8c11749cddd28bae6060c94ce6d6ac
IvanRosokha <54767244+rssites@users.noreply.github.com>
2022-02-23 18:02:35 +0200

    Update index.tsx

--- pluminite-ui/src/index.tsx
+++ pluminite-ui/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { App } from './components/app/App';
+import  App  from './components/app/App';
 import { initContracts } from './utils';
 import { NearContextProvider, NftContractContextProvider, MarketContractContextProvider } from './contexts';
 import { BrowserRouter } from "react-router-dom";
