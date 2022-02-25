78b680e915c87d61eabb914d1cf66e3dadabd47f
sergeyKokora <mail.uzhgorod@gmail.com>
2022-02-25 17:38:21 +0200

    чистка сміття

--- first_rust_project/target/debug/deps/first_rust_project-415ec3c751dfcc0e.d
+++
@@ -1,5 +0,0 @@
-c:\work\git\vinodex\first_rust_project\target\debug\deps\first_rust_project-415ec3c751dfcc0e.rmeta: src\main.rs
-
-c:\work\git\vinodex\first_rust_project\target\debug\deps\first_rust_project-415ec3c751dfcc0e.d: src\main.rs
-
-src\main.rs:
--- first_rust_project/target/debug/first_rust_project.d
+++
@@ -1,1 +0,0 @@
-C:\work\git\vinodex\first_rust_project\target\debug\first_rust_project.exe: C:\work\git\vinodex\first_rust_project\src\main.rs
