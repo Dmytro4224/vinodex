a5f815f2d93b04375de777c7a497a8a1506470f6
Erik Kopcha <erickopcha@gmail.com>
2022-03-04 15:49:42 +0200

    feat: social component

---
+++ pluminite-ui/src/components/social/Social.tsx
@@ -0,0 +1,40 @@
+import { Component } from "react";
+import { IBaseComponentProps, IProps, withComponent } from "../../utils/withComponent";
+import styles from './social.module.css';
+import facebookIcon from '../../assets/icons/facebook.svg';
+import twitterIcon from '../../assets/icons/twitter.svg';
+import googleIcon from '../../assets/icons/google.svg';
+import twitchIcon from '../../assets/icons/twitch.svg';
+
+interface ISocial extends IProps { }
+
+class Social extends Component<ISocial & IBaseComponentProps> {
+  render() {
+    return (
+      <ul className={styles.socialListWrap}>
+        <li>
+          <a href="#">
+            <img width="20" height="20" src={facebookIcon} alt="facebook" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img width="20" height="17" src={twitterIcon} alt="twitter" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img width="18" height="18" src={googleIcon} alt="google" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img width="18" height="19" src={twitchIcon} alt="twitch" />
+          </a>
+        </li>
+      </ul>
+    )
+  }
+}
+
+export default withComponent(Social);
