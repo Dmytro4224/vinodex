c1daa9ed76a51d4327e1b5195bf78f9c769c1cfd
ErikKopcha <erickopcha@gmail.com>
2022-03-23 12:52:09 +0200

    update: profile tokens, order detail

--- pluminite-ui/src/pages/orderDetail/orderDetail.tsx
+++ pluminite-ui/src/pages/orderDetail/orderDetail.tsx
@@ -2,9 +2,7 @@ import { Component } from "react";
 import TokenViewDetail from "../../components/tokenDetail/tokenViewDetail";
 import { IBaseComponentProps, IProps, withComponent } from "../../utils/withComponent";
 
-interface IOrderDetail extends IProps {
-    
-}
+interface IOrderDetail extends IProps {}
 
 class OrderDetail extends Component<IOrderDetail & IBaseComponentProps, {}, any> {
     public render(){
