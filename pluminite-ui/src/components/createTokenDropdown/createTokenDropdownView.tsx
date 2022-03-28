dd816d60baafff3743a09de6dacb3ef6d118f94b
ErikKopcha <erickopcha@gmail.com>
2022-03-28 11:42:07 +0300

    update

--- pluminite-ui/src/components/createTokenDropdown/createTokenDropdownView.tsx
+++ pluminite-ui/src/components/createTokenDropdown/createTokenDropdownView.tsx
@@ -19,9 +19,9 @@ class CreateTokenDropdownView extends Component<ICreateTokenDropdownView & IBase
     this.props.onNavigated && this.props.onNavigated();
 
     if (this.props.near.isAuth) {
-      if(isMultiple){
+      if (isMultiple){
         this.props.navigate('/create/multiple');
-      }else{
+      } else {
         this.props.navigate('/create/single');
       }
     } else {
@@ -29,35 +29,44 @@ class CreateTokenDropdownView extends Component<ICreateTokenDropdownView & IBase
     }
   }
 
-  render(){
-    return <Dropdown className={styles.dropWrap}>
-      <Dropdown.Toggle variant="" id="dropdown-basic" className={`${styles.isHiddenArrow} ${styles.goldFill} ${styles.fixMobileBtn}`}>
-        CREATE
-      </Dropdown.Toggle>
+  render() {
+    return (
+      <ButtonView
+        text={'CREATE'}
+        onClick={() => { this.createAction(false); }}
+        color={buttonColors.goldFill}
+        customClass={`ml-10px min-w-100px`}
+      />
 
-      <Dropdown.Menu>
-        <Dropdown.Item className={styles.item}>
-          <div className={`w-100 d-flex align-items-center justify-content-center flex-column ${styles.createDropdown}`}>
-            <div className={styles.mainTitleWrap}>
-              <h3 className={styles.mainTitle}>Create NFT</h3>
-              <p className={styles.mainSubTitle}>Choose “Single” if you want your collectible to be one of a find or “Multiple” if you want to sell one collectible multiple times.</p>
-            </div>
-            <div className={styles.createTypeWrap}>
-              <div onClick={() => { this.createAction(false) }} className={`${styles.single} ${styles.tokenWrap}`}>
-                <img src={singleIcon} alt=""/>
-                <p className={styles.line} />
-                <h5>Single</h5>
-              </div>
-              <div onClick={() => { this.createAction(true) }} className={`${styles.multiply} ${styles.tokenWrap}`}>
-                <img src={multiplyIcon} alt=""/>
-                <p className={styles.line} />
-                <h5>Multiple</h5>
-              </div>
-            </div>
-          </div>
-        </Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+      // <Dropdown className={styles.dropWrap}>
+      //   <Dropdown.Toggle variant="" id="dropdown-basic" className={`${styles.isHiddenArrow} ${styles.goldFill} ${styles.fixMobileBtn}`}>
+      //     CREATE
+      //   </Dropdown.Toggle>
+      //
+      //   <Dropdown.Menu>
+      //     <Dropdown.Item className={styles.item}>
+      //       <div className={`w-100 d-flex align-items-center justify-content-center flex-column ${styles.createDropdown}`}>
+      //         <div className={styles.mainTitleWrap}>
+      //           <h3 className={styles.mainTitle}>Create NFT</h3>
+      //           <p className={styles.mainSubTitle}>Choose “Single” if you want your collectible to be one of a find or “Multiple” if you want to sell one collectible multiple times.</p>
+      //         </div>
+      //         <div className={styles.createTypeWrap}>
+      //           <div onClick={() => { this.createAction(false) }} className={`${styles.single} ${styles.tokenWrap}`}>
+      //             <img src={singleIcon} alt=""/>
+      //             <p className={styles.line} />
+      //             <h5>Single</h5>
+      //           </div>
+      //           <div onClick={() => { this.createAction(true) }} className={`${styles.multiply} ${styles.tokenWrap}`}>
+      //             <img src={multiplyIcon} alt=""/>
+      //             <p className={styles.line} />
+      //             <h5>Multiple</h5>
+      //           </div>
+      //         </div>
+      //       </div>
+      //     </Dropdown.Item>
+      //   </Dropdown.Menu>
+      // </Dropdown>
+    )
   }
 }
 
