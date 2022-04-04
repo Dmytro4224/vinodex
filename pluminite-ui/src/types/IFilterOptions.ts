8b3b50c9412aecbfb989dcf6f35533bd3a9fc21b
ErikKopcha <erickopcha@gmail.com>
2022-04-04 17:46:57 +0300

    Update IFilterOptions.ts

--- pluminite-ui/src/types/IFilterOptions.ts
+++ pluminite-ui/src/types/IFilterOptions.ts
@@ -3,3 +3,14 @@ export interface IFilterOptions {
   priceFrom: number | null;
   priceTo: number | null;
 }
+
+// export interface IFilterOptions {
+// brand: string | null;
+// style: string | null;
+// year: string | null;
+// bottle_size: string | null;
+// type?: boolean | null;
+// priceFrom?: number | null;
+// priceTo?: number | null;
+// collection_id?: string | null;
+// }
